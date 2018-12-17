7deb313f53c6a591d822fb1906159ee70ccaa862
Vinay <Vinay.bv@accionlabs.com>
2018-12-17 18:15:18 +0530

    adde landing page

--- server.js
+++ server.js
@@ -4,13 +4,35 @@ var cors = require('cors');
 app.use(cors());
 var http = require('http').Server(app);
 var path = require('path');
-var port = (process.env.PORT || 4000);
+var port = (process.env.PORT || 4008);
 
-app.use(express.static(path.join(__dirname, '/build')));
 
-app.get('/*', function(req, res){
+app.use('/', express.static(path.join(__dirname, 'auto-grade-landing/')));
+app.use('/', express.static(path.join(__dirname, 'build/')));
+
+
+
+
+
+
+app.get('/', function(req, res){
+  // res.sendFile(__dirname + '/auto-grade-landing/index.html');
+  res.json("hello")
+});
+
+app.get('/login', function(req, res){
   res.sendFile(__dirname + '/build/index.html');
 });
+app.get('/app/', function(req, res){
+  res.sendFile(__dirname + '/build/index.html');
+});
+app.get('/app/*', function(req, res){
+  res.sendFile(__dirname + '/build/index.html');
+});
+
+
+
+
 
  http.listen(port, function(){
 				   console.log('listening on :' + port);
