713ea1fe2f72e49f764d53e66e769d11d6e29d69
Vinay <Vinay.bv@accionlabs.com>
2019-01-07 16:19:11 +0530

    added

--- server.js
+++ server.js
@@ -4,32 +4,21 @@ var cors = require('cors');
 app.use(cors());
 var http = require('http').Server(app);
 var path = require('path');
-var port = (process.env.PORT || 4008);
+var port = (process.env.PORT || 4021);
 
 
-app.use('/', express.static(path.join(__dirname, 'auto-grade-landing/')));
-app.use('/', express.static(path.join(__dirname, 'build/')));
-
-
+app.use(express.static(path.join(__dirname, 'build/')));
 
 
 
 
-app.get('/', function(req, res){
-  // res.sendFile(__dirname + '/auto-grade-landing/index.html');
-  res.json("hello")
-});
 
-app.get('/login', function(req, res){
+
+app.get('/*', function(req, res){
+  console.log('calling home page')
   res.sendFile(__dirname + '/build/index.html');
-});
-app.get('/app/', function(req, res){
-  res.sendFile(__dirname + '/build/index.html');
-});
-app.get('/app/*', function(req, res){
-  res.sendFile(__dirname + '/build/index.html');
-});
 
+});
 
 
 
